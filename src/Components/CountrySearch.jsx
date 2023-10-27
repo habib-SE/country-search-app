8325e99442542bc71bffd24b9d60d928f9bc4e39
habib-SE <hab67603@gmail.com>
2023-10-27 12:10:34 +0500

    update this file

--- src/Components/CountrySearch.jsx
+++ src/Components/CountrySearch.jsx
@@ -38,11 +38,16 @@ function CountrySearch() {
   };
 
   return (
-    <div>
-      <div className="mb-4">
+    <div className=" container bg-purple-100 w-full">
+      <div className=" flex justify-center items-center text-4xl bg-blue-500 text-white w-[106%]">
+        <ul className=" p-4  font-bold">
+          <li>Country Search App By Name</li>
+        </ul>
+      </div>
+      <div className="mb-4 py-9 bg-slate-300 w-[106%]">
         <input
           type="text"
-          className="px-3 py-2 border rounded-lg w-64"
+          className="px-10 py-2 border rounded-lg w-64"
           placeholder="Search for a country..."
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
@@ -54,21 +59,54 @@ function CountrySearch() {
           Search
         </button>
       </div>
-      <div>
-        <p className="mb-4"> {selectedCountry}</p>
+      <div className="">
+        <p className="mb-4 text-red-600 ml-10 font-serif text-2xl underline">
+          {" "}
+          {selectedCountry}
+        </p>
         {countryData && (
           <div>
             <h2 className="text-xl font-semibold mb-2">Country Information</h2>
-            <p>Country Name:<span className=" text-gray-400 ml-8">{countryData.name.common}</span> </p>
-            <p>Official Name: <span className=" text-gray-400 ml-8">{countryData.name.official}</span></p>
-            <p>In Urdu:  <span className=" text-gray-400 ml-[5%]">{countryData.translations.urd.official}</span></p>
-            <p>Capital: <span className=" text-gray-400 ml-20">{countryData.capital}</span></p>
-            <p>Population:<span className=" text-gray-400 ml-[4%]">{countryData.population}</span> </p>
-            <p>Languages: <span className=" text-gray-400 ml-[3%]">{Object.values(countryData.languages).join(", ")}</span></p>
+            <p>
+              Country Name:
+              <span className=" text-gray-400 ml-8">
+                {countryData.name.common}
+              </span>{" "}
+            </p>
+            <p>
+              Official Name:{" "}
+              <span className=" text-gray-400 ml-8">
+                {countryData.name.official}
+              </span>
+            </p>
+            <p>
+              In Urdu:{" "}
+              <span className=" text-gray-400 ml-[5%]">
+                {countryData.translations.urd.official}
+              </span>
+            </p>
+            <p>
+              Capital:{" "}
+              <span className=" text-gray-400 ml-20">
+                {countryData.capital}
+              </span>
+            </p>
+            <p>
+              Population:
+              <span className=" text-gray-400 ml-[4%]">
+                {countryData.population}
+              </span>{" "}
+            </p>
+            <p>
+              Languages:{" "}
+              <span className=" text-gray-400 ml-[3%]">
+                {Object.values(countryData.languages).join(", ")}
+              </span>
+            </p>
             <img
               src={countryData.flags.png}
               alt={`Flag of ${countryData.name.common}`}
-              className="w-20 h-auto ml-[4%] py-4"
+              className="w-20 h-auto ml-[3%] py-4"
             />
           </div>
         )}
